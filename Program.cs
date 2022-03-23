1615d946969484d35c73e518689abfa8bbe6f0c8
Anderson Straube <andersonstraube@gmail.com>
2022-03-23 12:02:53 -0300

    Initial code

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using StackExchange.Redis;
+using System;
+using System.Timers;
+using System.Threading.Tasks;
+
+namespace Redis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var redis = ConnectionMultiplexer.Connect("localhost,port: 6379,password=@anderson");
+            var db = redis.GetDatabase();
+            db.StringSet("message", "It's worked");
+            
+            Console.WriteLine(db.StringGet("message"));
+
+            Console.ReadLine();
+        }
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,15 @@
-# redis-container-and-dotnetcore-console
-redis-container-and-dotnetcore-console
+# Redis Container
+
+Redis running in a container and a .Net Core 6 console to test redis
+
+## Up container
+
+```
+docker-compose up -d
+```
+
+## Run .NET Core Console
+
+```
+dotnet run
+```
